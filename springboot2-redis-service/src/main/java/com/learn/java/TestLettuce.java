60e7e61a861e2e5d034cf9a6ea0b25b0ac5eab70
szd <15131232836@163.com>
2025-04-14 22:49:39 +0800

    feat: redis lettuce demo

--- springboot2-redis-service/src/main/java/com/learn/java/TestLettuce.java
+++ springboot2-redis-service/src/main/java/com/learn/java/TestLettuce.java
@@ -1,13 +1,22 @@
 package com.learn.java;
 
+import io.lettuce.core.KeyValue;
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.RedisURI;
 import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.reactive.RedisReactiveCommands;
 import io.lettuce.core.api.sync.RedisCommands;
 import org.junit.jupiter.api.Test;
 import io.lettuce.core.cluster.RedisClusterClient;
 import io.lettuce.core.cluster.api.StatefulRedisClusterConnection;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 报错：Caused by: java.lang.ClassNotFoundException: reactor.core.publisher.Sinks
+ * 版本冲突
+ */
 public class TestLettuce {
     @Test
     void test_connection() {
@@ -40,6 +49,33 @@ public class TestLettuce {
             //...
 
             connection.close();
+        }
+    }
+
+    @Test
+    void test_async() {
+        RedisClient redisClient = RedisClient.create("redis://localhost:6379");
+
+        try (StatefulRedisConnection<String, String> connection = redisClient.connect()) {
+            RedisReactiveCommands<String, String> reactiveCommands = connection.reactive();
+
+            // Reactively store & retrieve a simple string
+            reactiveCommands.set("foo", "bar").block();
+            reactiveCommands.get("foo").doOnNext(System.out::println).block(); // prints bar
+
+            // Reactively store key-value pairs in a hash directly
+            Map<String, String> hash = new HashMap<>();
+            hash.put("name", "John");
+            hash.put("surname", "Smith");
+            hash.put("company", "Redis");
+            hash.put("age", "29");
+            reactiveCommands.hset("user-session:124", hash)
+                            .then(reactiveCommands.hgetall("user-session:124")
+                                    .collectMap(KeyValue::getKey, KeyValue::getValue)
+                                    .doOnNext(System.out::println)).block();
+            // Prints: {surname=Smith, name=John, company=Redis, age=29}
+        } finally {
+            redisClient.shutdown();
         }
     }
 }
