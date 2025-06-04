93c4fe52e3b8b7659b11f98c3584a0e2e04a9e81
szd <15131232836@163.com>
2025-06-04 14:58:49 +0800

    feat: scala demo test

--- scala-service/src/main/scala/com/learn/scala/Hello.scala
+++ scala-service/src/main/scala/com/learn/scala/Hello.scala
@@ -5,9 +5,11 @@ object Hello {
   def main(args: Array[String]): Unit = {
     println("hello scala")
     System.out.println("hello scala")
+
     def ops(x: Int, y: Int, op: (Int, Int) => Int) = {
       op(x, y)
     }
+
     println(ops(1, 2, _ + _))
   }
 }
@@ -27,3 +29,29 @@ object TestWordCount {
   }
 }
 
+
+class TestCompare1[T: Ordering](obj1: T, obj2: T) {
+  def greater: T = {
+    val comparator = implicitly[Ordering[T]]
+    if (comparator.compare(obj1, obj2) > 0) obj1 else obj2
+  }
+}
+
+class TestCompare2[T](obj1: T, obj2: T)(implicit comp: Ordering[T]) {
+  def greater(): T = if (comp.compare(obj1, obj2) > 0) obj1 else obj2
+}
+
+
+import scala.math.Ordering.Implicits._
+
+class TestCompare3[T: Ordering](obj1: T, obj2: T) {
+  def greater: T = obj1.max(obj2) // 也可以写 if (obj1 > obj2)
+}
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    println(new TestCompare1(1, 2).greater)
+    println(new TestCompare2(1, 2).greater())
+    println(new TestCompare3(1, 2).greater)
+  }
+}
